02607adf049f66616ecfeda410be58a70ddd60be
Alex <alexdev0605@gmail.com>
2023-08-14 17:11:12 +0200

    fix: update user model and validation

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -1,11 +1,21 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  id: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  bio: String,
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
   image: String,
+  bio: String,
   threads: [
     {
       type: mongoose.Schema.Types.ObjectId,
@@ -16,9 +26,14 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
-  communities: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Community' }],
+  communities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Community',
+    },
+  ],
 });
 
-const User = mongoose.models.User || mongoose.model('User', userSchema); //avoir error on first loading
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 export default User;
